187a9fcd2862bcbbde62470af4db5463dfe54440
wizguin <16276645+wizguin@users.noreply.github.com>
2020-04-11 21:07:50 +0100

    Added styling and templating for threads

--- routes/thread.js
+++ routes/thread.js
@@ -6,8 +6,26 @@ import Database from '../database/Database'
 const router = express.Router()
 const database = new Database()
 
+
+async function renderThread(req, res) {
+    let threadId = req.params.thread
+    let post = await database.getPostById(threadId)
+
+    res.render('thread', {
+        title: `${post.text}`,
+        myUsername: req.session.username,
+        post: post
+    })
+}
+
+/*========== Get routes ==========*/
+
 router.get('/', function(req, res) {
     res.redirect('/home')
+})
+
+router.get('/:thread', function(req, res) {
+    renderThread(req, res)
 })
 
 
